58a2c2e494c86cd2b9e3e7ce7c0da98576cbb0df
schimar <martin.schilling@tyrolpath.at>
2023-10-09 15:01:27 +0200

    exonNo into header line, now for real

--- workflow/scripts/fusGetExons.py
+++ workflow/scripts/fusGetExons.py
@@ -67,9 +67,9 @@ if __name__ == "__main__":
             for line in fus:
                 line = line.strip('\n')
                 if line[0:2] == 'ge':
-                    print(line)
+                    #print(line)
                     header = '\t'.join(["gene1", "gene2", "strand1", "strand2", "breakpoint1", "breakpoint2", "exonNo1", "exonNo2", "site1", "site2", "typ", "split_reads1", "split_reads2", "disco_mates", "cov1", "cov2", "warning", "confidence", "reading_frame", "gene_id1", "gene_id2", "transcript_id1", "transcript_id2", "filters", "s2_gene", "s2_descr"])
-                    #print(len(header.split('\t')))
+                    print(header)
                 else:
                     lspl = line.split('\t')
                     [gene1, gene2, strand1, strand2, breakpoint1, breakpoint2, site1, site2, typ, split_reads1, split_reads2, disco_mates, cov1, cov2, warning, confidence, reading_frame, ensg1, ensg2, enst1, enst2, filters, s2_gene, s2_descr] = lspl
